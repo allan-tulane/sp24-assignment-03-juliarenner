c9cf612b833a52ccf5ac0e3624ab51a2ee02b13c
juliarenner <157319766+juliarenner@users.noreply.github.com>
2024-04-30 01:33:38 +0000

    Assignment 03

--- answers.md
+++ answers.md
@@ -1,8 +1,32 @@
 # CMPS 2200 Assignment 3
 ## Answers
 
-**Name:**_________________________
+**Name:** Julia Renner
 
 
 Place all written answers from `assignment-03.md` here for easier grading.
 
+- 1(a):
+
+- A greedy algorithm that produces as few coins as possible that sum to N must first choose the largest value of 2^k (coin value) that is not greater than N. Then, the algorithm should continue subtracting this value from N until N equals zero. 
+
+- 1(b):
+
+- The largest coin possible is being used for the subtraction which means that there could be no way for a more optimal solution to exist because the least coins possible could be used in this greedy case.
+
+- 1(c):
+
+- The Work of my algorithm is O(logn)
+- The Span of my algorithm is O(logn)
+
+- 2(a):
+
+- If the Fortuito currency uses coins with values 1, 3, and 4, the greedy algorithm would likely choose a 4 dollar coin and two one dollar coins if it were to be making change for 6 Fortuitan coins. This is obviously not the most optimal solution, which would be to simply select two of the 3 dollar coins. In this case, the greedy algorithm fails to use the fewest coins.
+
+- 2(b):
+
+- This problem has an optimal substructure property because it finds the optimal solutions to the sub-problems less than N amounts in order to solve the optimal amount for N.
+
+- 2(c):
+
+- We could use dynamic programming for this problem by writing a recurisve algorithm that iterates over itself, storing results from subproblems and thereby preventing uneeded repetition. This would be using top-down memoization. Work = O(N*k), Span = O(N).
--- main.py
+++ main.py
@@ -1,29 +1,63 @@
-import math, queue
+import math
+import queue
 from collections import Counter
 
-####### Problem 3 #######
+test_cases = [('book', 'back'), ('kookaburra', 'kookybird'),
+              ('elephant', 'relevant'), ('AAAGAATTCA', 'AAATCA')]
+alignments = [('b--ook', 'bac--k'), ('kook-ab-urr-a', 'kooky-bi-r-d-'),
+              ('relev--ant', '-ele-phant'), ('AAAGAATTCA', 'AAA---T-CA')]
 
-test_cases = [('book', 'back'), ('kookaburra', 'kookybird'), ('elephant', 'relevant'), ('AAAGAATTCA', 'AAATCA')]
-alignments = [('b--ook', 'bac--k'), ('kook-ab-urr-a', 'kooky-bi-r-d-'), ('relev--ant','-ele-phant'), ('AAAGAATTCA', 'AAA---T-CA')]
 
 def MED(S, T):
-    # TO DO - modify to account for insertions, deletions and substitutions
-    if (S == ""):
-        return(len(T))
-    elif (T == ""):
-        return(len(S))
+  if (S == ""):
+    return (len(T))
+  elif (T == ""):
+    return (len(S))
+  else:
+    if (S[0] == T[0]):
+      return (MED(S[1:], T[1:]))
     else:
-        if (S[0] == T[0]):
-            return(MED(S[1:], T[1:]))
-        else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
+      return (1 + min(MED(S, T[1:]), MED(S[1:], T)))
 
 
 def fast_MED(S, T, MED={}):
-    # TODO -  implement top-down memoization
-    pass
+  if (S, T) in MED:
+    return MED[(S, T)]
+
+  if S == "":
+    return len(T)
+
+  elif T == "":
+    return len(S)
+  elif S[0] == T[0]:
+    result = fast_MED(S[1:], T[1:], MED)
+  else:
+    cost_insert = fast_MED(S, T[1:], MED)
+    cost_delete = fast_MED(S[1:], T, MED)
+    result = 1 + min(cost_insert, cost_delete)
+    MED[(S, T)] = result
+  return result
+
 
 def fast_align_MED(S, T, MED={}):
-    # TODO - keep track of alignment
-    pass
-
+  if (S, T) in MED:
+    return MED[(S, T)]
+  if S == "":
+    MED[(S, T)] = ("-" * len(T), T)
+    return MED[(S, T)]
+  if T == "":
+    MED[(S, T)] = (S, "-" * len(S))
+    return MED[(S, T)]
+  if S[0] == T[0]:
+    S_aligned, T_aligned = fast_align_MED(S[1:], T[1:], MED)
+    MED[(S, T)] = (S[0] + S_aligned, T[0] + T_aligned)
+  else:
+    S_insert, T_insert = fast_align_MED(S, T[1:], MED)
+    S_delete, T_delete = fast_align_MED(S[1:], T, MED)
+    insertcost = 1 + len(S_insert)
+    deletecost = 1 + len(S_delete)
+    if insertcost <= deletecost:
+      MED[(S, T)] = ("-" + S_insert, T[0] + T_insert)
+    else:
+      MED[(S, T)] = (S[0] + S_delete, "-" + T_delete)
+  return MED[(S, T)]
